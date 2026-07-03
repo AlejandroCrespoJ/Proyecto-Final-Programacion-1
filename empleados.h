853117d50fd8305bab4a3510eb5aefc06dfe2c86
Alejandro Crespo <alejandro.crespo@udla.edu.ec>
2026-07-03 00:28:08 -0500

    creacion de funciones para usar en empleados.c y en main.c

--- empleados.h
+++ empleados.h
@@ -15,3 +15,25 @@ typedef struct {
     float sueldo_base;
     int horas_extra;
 } Empleado;
+
+void mostrarMenu(void);
+
+int cargarEmpleados(Empleado empleados[], int *cantidad, const char *nombreArchivo);
+int guardarEmpleados(const Empleado empleados[], int cantidad, const char *nombreArchivo);
+
+void registrarEmpleado(Empleado empleados[], int *cantidad);
+void listarEmpleados(const Empleado empleados[], int cantidad);
+void buscarEmpleado(const Empleado empleados[], int cantidad);
+void actualizarEmpleado(Empleado empleados[], int cantidad);
+void eliminarEmpleado(Empleado empleados[], int *cantidad);
+
+float calcularSueldoTotal(const Empleado *empleado);
+
+int leerEntero(const char *mensaje, int minimo);
+float leerFloat(const char *mensaje, float minimo);
+void leerCadena(const char *mensaje, char destino[], int tamano);
+void limpiarCadena(char cadena[]);
+
+int codigoValido(const char *codigo);
+int existeCodigo(const Empleado empleados[], int cantidad, const char *codigo);
+int contieneTexto(const char *texto, const char *busqueda);
