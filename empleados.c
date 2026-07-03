8bd10e5a4464f915e1d14168191020b02ca36f6a
andres-cabrera19 <andres.cabrera.calupina@udla.edu.ec>
2026-07-03 00:18:28 -0500

    primer commit inicio

--- empleados.c
+++ empleados.c
@@ -3,3 +3,18 @@
 #include <string.h>
 #include <ctype.h>
 #include "empleados.h"
+
+static void limpiarBufferEntrada(void) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+    }
+}
+
+static int tieneComa(const char *cadena) {
+    return strchr(cadena, ',') != NULL;
+}
+
+static void copiarSeguro(char destino[], const char *origen, int tamano) {
+    strncpy(destino, origen, tamano - 1);
+    destino[tamano - 1] = '\0';
+}
