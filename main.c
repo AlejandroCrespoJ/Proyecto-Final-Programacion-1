0249aa7fbf5eea64ceedbb692e9102713ab31d96
Alejandro Crespo <alejandro.crespo@udla.edu.ec>
2026-07-03 01:12:25 -0500

    creacion de switch para determinar las opciones disponibles del menu principal. con una opcion al final para evitar que eligan el numero 0

--- main.c
+++ main.c
@@ -14,7 +14,42 @@ int main(void) {
     }
 
 
+do {
+        mostrarMenu();
+        opcion = leerEntero("Seleccione una opcion: ", 0);
 
+        switch (opcion) {
+            case 1:
+                registrarEmpleado(empleados, &cantidad);
+                break;
+            case 2:
+                listarEmpleados(empleados, cantidad);
+                break;
+            case 3:
+                buscarEmpleado(empleados, cantidad);
+                break;
+            case 4:
+                actualizarEmpleado(empleados, cantidad);
+                break;
+            case 5:
+                eliminarEmpleado(empleados, &cantidad);
+                break;
+            case 6:
+                if (guardarEmpleados(empleados, cantidad, ARCHIVO_EMPLEADOS)) {
+                    printf("Cambios guardados correctamente en %s.\n", ARCHIVO_EMPLEADOS);
+                }
+                break;
+            case 0:
+                if (guardarEmpleados(empleados, cantidad, ARCHIVO_EMPLEADOS)) {
+                    printf("Cambios guardados automaticamente en %s.\n", ARCHIVO_EMPLEADOS);
+                }
+                printf("Programa finalizado.\n");
+                break;
+            default:
+                printf("Opcion invalida. Intente nuevamente.\n");
+                break;
+        }
+    } while (opcion != 0);
 
 
 }
