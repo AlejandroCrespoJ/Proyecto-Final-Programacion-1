68b5a24e6425b13bd648d2bbcc8f8658ddef5b15
Alejandro Crespo <alejandro.crespo@udla.edu.ec>
2026-07-03 00:15:31 -0500

    define restantes en el

--- empleados.h
+++ empleados.h
@@ -1,2 +1,9 @@
 #ifndef EMPLEADOS_H
 #define EMPLEADOS_H
+
+#define MAX_EMPLEADOS 100
+#define MAX_CODIGO 16
+#define MAX_NOMBRE 60
+#define MAX_CARGO 40
+#define ARCHIVO_EMPLEADOS "empleados.csv"
+#define VALOR_HORA_EXTRA 5.0f
