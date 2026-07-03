8912ae73b03e4fc66762e6c5b4105ab1d7632edf
andres-cabrera19 <andres.cabrera.calupina@udla.edu.ec>
2026-07-03 00:39:42 -0500

    uso de fgets

--- empleados.c
+++ empleados.c
@@ -17,4 +17,26 @@ static int tieneComa(const char *cadena) {
 static void copiarSeguro(char destino[], const char *origen, int tamano) {
     strncpy(destino, origen, tamano - 1);
     destino[tamano - 1] = '\0';
+}
 
+static void leerCadenaOpcional(const char *mensaje, char destino[], int tamano) {
+    int textoLargo;
+
+    do {
+        printf("%s", mensaje);
+
+        if (fgets(destino, tamano, stdin) == NULL) {
+            destino[0] = '\0';
+            return;
+        }
+
+        textoLargo = strchr(destino, '\n') == NULL;
+        if (textoLargo) {
+            limpiarBufferEntrada();
+            printf("Entrada demasiado larga. Intente nuevamente.\n");
+            destino[0] = '\0';
+        } else {
+            limpiarCadena(destino);
+        }
+    } while (textoLargo);
+}
